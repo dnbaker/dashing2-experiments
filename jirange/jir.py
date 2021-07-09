665a1eb7f362fca9c2cabd7146bf044d44a69ce6
Daniel N. Baker <dnb@cs.jhu.edu>
2021-07-09 15:16:07 -0400

    New

--- jirange/jir.py
+++ jirange/jir.py
@@ -2,16 +2,9 @@ import numpy as np
 import scipy.sparse as sp
 import sys
 import subprocess
+import argparse
 
 
-# 1. Parse the ids
-# 2. For each relevant id, perform full k-mer count dictionary generation
-# 3. Then, we can use this to calculate shared k-mers, weighted k-mer comparisons.
-# 4. Then, we calculate all-pairs over the relevant genomes for all of these.
-#   1. This includes exact distances, the range of setsketch parameters, HLL, Mash, Dashing1, bindash, ARI
-# 5. Then, parse those results out and prepare the table or results
-
-# This will give us ANI, jaccard, etc. and will do super awesome powerful stuff
 
 def parse_bf(path):
     import itertools
@@ -21,6 +14,7 @@ def parse_bf(path):
     tups = []
     for line in open(path):
         toks = line.strip().split()
+        print(toks, len(toks))
         bkts.append(int(toks[1]))
         rngs.append(list(map(float, toks[2].strip("[]").split("->"))))
         tups.append([list(map(int, x.split(":"))) for x in toks[3:]])
@@ -88,7 +82,13 @@ def bbminhash_jaccard(p1, p2, size, k=17, bbnb=32, fss=False):
 
 
 if __name__ == "__main__":
-    parse_bf(sys.argv[1] if sys.argv[1:] else "selected_buckets_100.txt")
     if(sys.argv[2:]):
-        parsedata(sys.argv[1], sys.argv[2])
-
+        ap = argparse.ArgumentParser()
+        ap.add_argument("table", help="Path to a list of selected genomes with ranged Jaccards")
+        ap.add_argument("fnames", help="Path to a list of selected genomes")
+        args = ap.parse_args()
+        res = parsedata(args.table, args.fnames)
+        bkts, rngs, tups, total_ids = res
+        print(list(map(lambda x: str(x.shape), res)))
+    else:
+        parse_bf(sys.argv[1] if sys.argv[1:] else "selected_buckets_100.txt")
