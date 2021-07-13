11d74dd4012b6f0435d834d6091af35b0c4e981f
dnbh <dnb@cs.jhu.edu>
2021-07-13 19:41:54 -0400

    Updates and corrections

--- jirange/jir.py
+++ jirange/jir.py
@@ -37,6 +37,7 @@ def parse_bf(path):
             rngs[i].append(rngs[i][0])
     assert len(set(map(len, rngs))) == 1
     rngs = np.array(rngs)
+    print(f"Total {len(total_ids)} ids parsed", file=sys.stderr)
     return list(map(np.array, (bkts, rngs, tups, total_ids)))
 
 
@@ -57,8 +58,8 @@ def getani(l, r):
     return np.fromiter(check_output(cmd).decode().strip().split("\t")[-3:], dtype=np.float64)
 
 
-def getmashji(l, r, *, k, sz=1024):
-    return float(check_output(f"jaccard_mash dist -s {sz} -k {k} -j -t {l} {r}").decode().strip().split("\n")[1].split()[-1])
+def getmashji(l, r, *, k, size=1024):
+    return float(check_output(f"jaccard_mash dist -s {size} -k {k} -j -t {l} {r}").decode().strip().split("\n")[1].split()[-1])
 
 
 def getdashingji(l, r, *, k, l2s=10):
@@ -78,36 +79,37 @@ def setsketch_jaccard(p1, p2, size, k=17, nb=8, fss=False, executable="dashing2"
        nb is 8 by default, which is Dashing 2's typical size, though dashing-f and dashing-ld use 4 bytes and 16 bytes
        to store float32 and long double hash registers, respectively.
     '''
+    fcstr = f"--fastcmp {nb}" if nb < 8 else ""
     return float(check_output(f"{executable} sketch -k{k} {'--full-setsketch ' if fss else ''} -S{size} --fastcmp {nb} --cache --cmpout /dev/stdout {p1} {p2}").decode().strip('\n').split('\n')[-2].split("\t")[1])
 
 
-def bbminhash_jaccard(p1, p2, size, k=17, bbnb=32, fss=False, executable="dashing2"):
+def bbminhash_jaccard(p1, p2, size, k=17, nb=32, fss=False, executable="dashing2"):
     '''k is the k to use, and nb is the number of bytes to use in the setsketch-based set similarity estimation
-       bbnb is 8 by default, which is Dashing 2's typical size
+       nb is 8 by default, which is Dashing 2's typical size
     '''
-    nb = bbnb // 8
-    assert (bbnb & (bbnb - 1)) == 0 and 4 <= bbnb <= 64, "bbnb, number of bits for bbit minhash, should be 4, 8, 16, 32, or 64"
-    return float(check_output(f"{executable} sketch -k{k} {'--full-setsketch ' if fss else ''} --bbit-sigs -S{size} --fastcmp {nb} --cache --cmpout /dev/stdout {p1} {p2}").decode().strip('\n').split('\n')[-2].split("\t")[1])
+    assert (nb & (nb - 1)) == 0 and 4 <= nb <= 64, "nb, number of bits for bbit minhash, should be 4, 8, 16, 32, or 64"
+    fcstr = f"--fastcmp {nb / 8}"
+    return float(check_output(f"{executable} sketch -k{k} {'--full-setsketch ' if fss else ''} --bbit-sigs -S{size} {fcstr} --cache --cmpout /dev/stdout {p1} {p2}").decode().strip('\n').split('\n')[-2].split("\t")[1])
 
 
-def getall(l, r, k=17, sz=1024, executable="dashing2"):
+def getall(l, r, k=17, size=1024, executable="dashing2"):
     '''
-        for values of k, sz, and executable, return
+        for values of k, size, and executable, return
         all similarity comparisons using Mash, Dashing, Dashing2, and fastANI
     '''
-    return np.array([getmashji(l, r, k=k, sz=sz),
-                     getdashingji(l, r, k=k, l2s=int(np.log2(sz))),
+    return np.array([getmashji(l, r, k=k, size=size),
+                     getdashingji(l, r, k=k, l2s=int(np.log2(size))),
                      exact_jaccard(l, r),
-                     exact_wjaccard(l, r),
-                     getani(l, r)] +
-                     [setsketch_jaccard(l, r, k=k, nb=nb, fss=False, executable=executable) for nb in (8, 4, 2, 1)] +
-                     [bbminhash_jaccard(l, r, k=k, nb=nb, fss=True, executable=executable) for nb in (8, 4, 2, 1)], np.float32)
+                     exact_wjaccard(l, r)] + 
+                     # getani(l, r) +
+                     [setsketch_jaccard(l, r, size=size, k=k, nb=nb, fss=False, executable=executable) for nb in (8, 2, 1, .5)] +
+                     [bbminhash_jaccard(l, r, size=size, k=k, nb=int(nb * 8), fss=True, executable=executable) for nb in (8, 4, 2, 1, .5)], np.float32)
 
 def packed(x):
-    l, r, k, sz, executable = x
-    return getall(l, r, k=k, sz=sz, executable=executable)
+    l, r, k, size, executable = x
+    return getall(l, r, k=k, size=size, executable=executable)
 
-def pargetall(tups, k=17, sz=1024, executable="dashing2", cpu=-1):
+def pargetall(tups, k=17, executable="dashing2", cpu=-1):
     import multiprocessing as mp
     if cpu < 0: cpu = mp.cpu_count()
     with mp.Pool(cpu) as p:
@@ -138,16 +140,18 @@ if __name__ == "__main__":
             # print(tup.shape)
             s += ",".join("%s-%s" % (x[0], x[1]) for x in tup)
             s += '\n'
-        hv = hash(",".join(sys.argv))
+        hv = abs(hash(",".join(sys.argv)))
         rng = range(args.s, args.S, args.T)
         sdict = {"k": k, "executable": args.executable, "cpu": args.cpu}
-        tups = [(full_genome_ids[l], full_genome_ids[r], k, 1 << sz, args.executable) for l, r in tups.reshape(-1, 2) for sz in rng]
-        fullmat = np.stack([pargetall(tups, sz=sz, **sdict) for sz in rng])
-        fullmat.astype(np.float32).tofile("fullmat.f32.%d.%s" % (hv, fullmat.shape))
+        tups = [(full_genome_ids[l], full_genome_ids[r], k, 1 << size, args.executable) for l, r in tups.reshape(-1, 2) for size in rng]
+        print(f"Generated {len(tups)} tuples, which are being passed to pargetall", file=sys.stderr)
+        fullmat = np.stack(pargetall(tups, **sdict))
+        fs = str(fullmat.shape).replace(" ", "").replace(",", "-")
+        fullmat.astype(np.float32).tofile("fullmat.f32.%d.%s" % (hv, fs))
         with open("settings.%d.txt" % hv, "w") as f:
             for st in tups:
-                l, r, k, sz = st
-                print("%s\t%s\t\%d\t%d\n" % (l, r, k, sz), file=f)
+                l, r, k, size, _ = st
+                print("%s\t%s\t%d\t%d\n" % (l, r, k, size), file=f)
     else:
         print("Running tests, not running experiment", file=sys.stderr)
         parse_bf(sys.argv[1] if sys.argv[1:] else "selected_buckets_100.txt")
