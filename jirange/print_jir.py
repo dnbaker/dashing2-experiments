b83cad840e124d7ab0b08acdd20e00f15c8ef0eb
dnbh <dnb@cs.jhu.edu>
2021-07-19 17:09:25 -0400

    Add One-perm vs full setsketch

--- jirange/print_jir.py
+++ jirange/print_jir.py
@@ -1,9 +1,11 @@
 from sys import argv, stdout, stderr
 import numpy as np
 import argparse
-header = "#ANI\tWJI\tJI\tMash\tDash1\tBD8\tBD4\tBD2\tBD1\tBDN\tSS8\tSS2\tSS1\tSSN\tFSS8\tFSS2\tFSS1\tFSSN\tMH8\tMH4\tMH2\tMH1\tMHN"
+# header = "#ANI\tWJI\tJI\tMash\tDash1\tBD8\tBD4\tBD2\tBD1\tBDN\tSS8\tSS2\tSS1\tSSN\tFSS8\tFSS2\tFSS1\tFSSN\tMH8\tMH4\tMH2\tMH1\tMHN"
+# Newest version, including full and one-perm
+header = "#ANI\tWJI\tJI\tMash\tDash1\tBD8\tBD4\tBD2\tBD1\tBDN\tSS8\tSS2\tSS1\tSSN\tFSS8\tFSS2\tFSS1\tFSSN\tMH8\tMH4\tMH2\tMH1\tMHN\tFMH8\tFMH4\tFMH2\tFMH1\tFMHN"
 ap = argparse.ArgumentParser()
-ap.add_argument("matrix", help="Emitted numpy array from jirange/jir.py, which is (x by 23) in shape")
+ap.add_argument("matrix", help="Emitted numpy array from jirange/jir.py, which is (x by 28) in shape")
 ap.add_argument("settings", help="Emitted results from jirange/jir.py, which has tuples for the experimental conditions")
 ap = ap.parse_args()
 
