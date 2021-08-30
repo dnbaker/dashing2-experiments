4af31084dd0976a0589c5b856f31c6655cb5c51e
Daniel N. Baker <dnb@cs.jhu.edu>
2021-08-30 15:53:55 -0400

    Add script

---
+++ allpairs/allpairs.py
@@ -0,0 +1,26 @@
+
+
+# Call all-pairs
+
+def allpairs_mash(pathf, k, threads):
+    return 0
+def allpairs_dashing(pathf, k, threads):
+    return 0
+def allpairs_dashing2(pathf, k, threads):
+    return 0
+def allpairs_bindash(pathf, k, threads):
+    return 0
+
+def allpairs_jthresh_mash(pathf, k, threads, *, j):
+    assert 0. < j < 1.
+    # Perform all-pairs distance using Mash, filtered
+    return 0
+def allpairs_jthresh_dashing(pathf, k, threads, *, j):
+    assert 0. < j < 1.
+    return 0
+def allpairs_jthresh_dashing2(pathf, k, threads, *, j):
+    assert 0. < j < 1.
+    return 0
+def allpairs_jthresh_bindash(pathf, k, threads, *, j):
+    assert 0. < j < 1.
+    return 0
