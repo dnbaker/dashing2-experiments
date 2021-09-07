4900c269ffbe7f0bdc9c49e647d6edc56b36b01a
Daniel Baker <dnb@cs.jhu.edu>
2021-09-07 16:31:56 -0400

    Work on all-pairs experiment

--- allpairs/allpairs.py
+++ allpairs/allpairs.py
@@ -1,26 +1,114 @@
+import sys
+import numpy as np
+import subprocess
+from time import time
 
 
-# Call all-pairs
+def bch_sketch_mash(pathf, k, threads, destp, size):
+    startt = time()
+    subprocess.check_call(f"mash sketch -k {k} -o {destp} -l {pathf} -p {threads}", shell=True)
+    stopt = time()
+    return destp, (stopt - startt)
 
-def allpairs_mash(pathf, k, threads):
-    return 0
-def allpairs_dashing(pathf, k, threads):
-    return 0
-def allpairs_dashing2(pathf, k, threads):
-    return 0
-def allpairs_bindash(pathf, k, threads):
+
+def bch_sketch_dashing(pathf, k, threads, size):
+    startt = time()
+    subprocess.check_call(f"dashing sketch -S {int(np.ceil(np.log2(size)))} -k {k} -F {pathf} -p {threads}", shell=True)
+    stopt = time()
+    return pathf, (stopt - startt)
+
+
+def bch_sketch_dashing2(pathf, k, threads, size, oneperm=False):
+    startt = time()
+    pstr = " --oph " if oneperm else ""
+    subprocess.check_call(f"dashing2 sketch -k {k} -S {size} -F {pathf} -p {threads} {pstr}", shell=True)
+    return pathf, (time() - startt)
+
+
+def bch_dist_dashing2(pathf, k, threads, size, oneperm=False, distdest=None, binary=False, regsize=-1):
+    if distdest is None:
+        raise RuntimeError("Must provide distdest to bch_dist_dashing2")
+    if regsize <= 0. or regsize not in [0.5, 1, 2, 4, 8]:
+        raise ValueError("regsize must be > 0 and in [.5, 1, 2, 4, 8]")
+    startt = time()
+    pstr = " --oph " if oneperm else ""
+    pstr += " --binary-output " if binary else ""
+    subprocess.check_call(f"dashing2 sketch -k {k} --cmpout {distdest} -S {size} -F {pathf} -p {threads} {pstr}", shell=True)
+    return pathf, (time() - startt)
+
+
+def bch_dist_dashing(pathf, k, threads, size, distdest=None, binary=False):
+    if distdest is None:
+        raise RuntimeError("Must provide distdest to bch_dist_dashing2")
+    startt = time()
+    pstr = " --emit-binary" if binary else ""
+    subprocess.check_call(f"dashing dist -k {k} -O{distdest} -o {distdest + '.sizes'} -S {int(np.ceil(np.log2(size)))} -F {pathf} -p {threads} {pstr}", shell=True)
+    return pathf, (time() - startt)
+
+
+def bch_dist_bindash(pathf, threads, size, distdest=None):
+    if distdest is None:
+        raise RuntimeError("Must provide distdest to bch_dist_dashing2")
+    startt = time()
+    subprocess.check_call(f"bindash dist --outfname={distdest} --nthreads={threads} {pathf}", shell=True)
+    return pathf, (time() - startt)
+
+
+def bch_dist_mash(pathf, threads, distdest=None):
+    if distdest is None:
+        raise RuntimeError("Must provide distdest to bch_dist_dashing2")
+    startt = time()
+    subprocess.check_call(f"mash dist -p {threads} {pathf} > {distdest}", shell=True)
+    return pathf, (time() - startt)
+
+
+def bch_sketch_bindash(pathf, k, threads, *, destp, bbits, size):
+    startt = time()
+    subprocess.check_call(f"bindash sketch --kmerlen={k} --sketchsize64={size//64} --listfname={pathf} --outfname={destp} --nthreads={threads}", shell=True)
+    return destp, (time() - startt)
+
+
+def repeat_x(func, ntimes, *args, **kwargs):
+    timevec = np.zeros(ntimes)
+    for i in range(ntimes):
+        res = func(*args, **kwargs)
+        timevec[i] = res[-1]
+    return (res[0], np.median(timevec))
+
+
+def test():
+    print(bch_sketch_mash("fn.txt", 31, 1, "mashdest.msh", 1024))
+    bch_dist_mash("mashdest.msh", 1, "mashDistance.msh")
+    bdpath, bdtime = bch_sketch_bindash("fn.txt", 31, 1, size=1024, destp="bdshdest.bdsh", bbits=32)
+    bch_dist_bindash(bdpath, 1, 1024, "bdshDistance.bdsh")
+    print(bch_dist_dashing2("fn.txt", 31, 1, 1024, distdest="d1dist.bin", binary=True, regsize=8))
+    print(bch_dist_dashing("fn.txt", 31, 1, 1024, distdest="d2dist.bin", binary=True))
+    print(bch_sketch_mash("fn.txt", 31, 1, "mashdest.msh", 1024))
+    print(bch_sketch_dashing("fn.txt", 31, 1, 1024))
+    print(bch_sketch_dashing2("fn.txt", 31, 1, 1024))
+    print(bch_sketch_dashing2("fn.txt", 31, 1, 1024, oneperm=True))
+    print(bdpath, bdtime)
+    mmed = repeat_x(bch_sketch_mash, 3, "fn.txt", 31, 1, "mashdest.msh", 1024)
+    print(mmed)
+
+
+def main():
+    from argparse import ArgumentParser as AP
+    ap = AP()
+    ap.add_argument("--nrepeat", type=int, default=3)
+    ap.add_argument("--nthreads", type=int, default=1)
+    ap.add_argument("-k", type=int, default=31)
+    ap.add_argument("fnames", type=str)
+    args = ap.parse_args()
+    nt = args.nthreads
+    fn = args.fnames
+    ntimes = args.nrepeat
+    print(f"##Results for {fn}")
+    print("#Method\tk\tSketchTime\tDistanceTime")
     return 0
 
-def allpairs_jthresh_mash(pathf, k, threads, *, j):
-    assert 0. < j < 1.
-    # Perform all-pairs distance using Mash, filtered
-    return 0
-def allpairs_jthresh_dashing(pathf, k, threads, *, j):
-    assert 0. < j < 1.
-    return 0
-def allpairs_jthresh_dashing2(pathf, k, threads, *, j):
-    assert 0. < j < 1.
-    return 0
-def allpairs_jthresh_bindash(pathf, k, threads, *, j):
-    assert 0. < j < 1.
-    return 0
+if __name__ == "__main__":
+    if not sys.argv[1:]:
+        test()
+    else:
+        main()
