e9bf225d7d6e6e1182cfeb083d6d1fc28c32c770
Daniel Baker <dnb@cs.jhu.edu>
2021-09-10 12:02:37 -0400

    Tweaks to allpairs experiment - results should be coming in soon.

--- allpairs/allpairs.py
+++ allpairs/allpairs.py
@@ -34,7 +34,7 @@ def bch_dist_dashing2(pathf, k, threads, size, oneperm=False, distdest=None, bin
     startt = time()
     pstr = " --oph " if oneperm else ""
     pstr += " --binary-output " if binary else ""
-    subprocess.check_call(f"dashing2 sketch -k {k} --cmpout {distdest} -S {size} -F {pathf} -p {threads} {pstr}", shell=True)
+    subprocess.check_call(f"dashing2 sketch -k {k} --cache --cmpout {distdest} -S {size} -F {pathf} -p {threads} {pstr}", shell=True)
     return pathf, (time() - startt)
 
 
@@ -43,7 +43,7 @@ def bch_dist_dashing(pathf, k, threads, size, distdest=None, binary=False):
         raise RuntimeError("Must provide distdest to bch_dist_dashing2")
     startt = time()
     pstr = " --emit-binary" if binary else ""
-    subprocess.check_call(f"dashing dist -k {k} -O{distdest} -o {distdest + '.sizes'} -S {int(np.ceil(np.log2(size)))} -F {pathf} -p {threads} {pstr}", shell=True)
+    subprocess.check_call(f"dashing dist --cache-sketches -k {k} -O{distdest} -o {distdest + '.sizes'} -S {int(np.ceil(np.log2(size)))} -F {pathf} -p {threads} {pstr}", shell=True)
     return pathf, (time() - startt)
 
 
@@ -78,18 +78,18 @@ def repeat_x(func, ntimes, *args, **kwargs):
 
 
 def test():
-    print(bch_sketch_mash("fn.txt", 31, 1, "mashdest.msh", 1024))
-    bch_dist_mash("mashdest.msh", 1, "mashDistance.msh")
+    print(bch_sketch_mash("fn.txt", 31, 1, "MASHdest.msh", 1024))
+    bch_dist_mash("MASHdest.msh", 1, "mashDistance.msh")
     bdpath, bdtime = bch_sketch_bindash("fn.txt", 31, 1, size=1024, destp="bdshdest.bdsh", bbits=32)
     bch_dist_bindash(bdpath, 1, 1024, "bdshDistance.bdsh")
     print(bch_dist_dashing2("fn.txt", 31, 1, 1024, distdest="d1dist.bin", binary=True, regsize=8))
     print(bch_dist_dashing("fn.txt", 31, 1, 1024, distdest="d2dist.bin", binary=True))
-    print(bch_sketch_mash("fn.txt", 31, 1, "mashdest.msh", 1024))
+    print(bch_sketch_mash("fn.txt", 31, 1, "MASHdest.msh", 1024))
     print(bch_sketch_dashing("fn.txt", 31, 1, 1024))
     print(bch_sketch_dashing2("fn.txt", 31, 1, 1024))
     print(bch_sketch_dashing2("fn.txt", 31, 1, 1024, oneperm=True))
     print(bdpath, bdtime)
-    mmed = repeat_x(bch_sketch_mash, 3, "fn.txt", 31, 1, "mashdest.msh", 1024)
+    mmed = repeat_x(bch_sketch_mash, 3, "fn.txt", 31, 1, "MASHdest.msh", 1024)
     print(mmed)
     return 0
 
@@ -98,27 +98,50 @@ def main():
     from argparse import ArgumentParser as AP
     ap = AP()
     ap.add_argument("--nrepeat", type=int, default=3)
-    ap.add_argument("--nthreads", type=int, default=1)
-    ap.add_argument("--sketchsize", type=int, default=0)
-    ap.add_argument("-k", type=int, default=31)
+    ap.add_argument("--nthreads", '-p', action='append')
+    ap.add_argument("--sketchsize", '-s', action='append')
+    ap.add_argument("-k", action='append')
     ap.add_argument("fnames", type=str)
     args = ap.parse_args()
-    ssz = args.sketchsize
-    if ssz <= 0:
-        raise ValueError("sketchsize must be > 0")
-    elif (ssz - 1) & ssz:
-        raise ValueError("Sketchsize must be a power of 2 for this experiment")
-    nt = args.nthreads
+    if not args.nthreads:
+        args.nthreads = [1]
     fn = args.fnames
+    print(f"##Options: {args}")
+    print(f"##Results for {fn}")
+    print("#Method\tk\tNumReg\tRegisterSize\tSketchTime\tDistanceTime")
+    if not args.sketchsize:
+        raise ValueError("Required: at least one value for sketchsize")
+    if not args.k:
+        raise ValueError("Required: at least one value for k")
+    sszes = list(map(int, args.sketchsize))
+    if any(x <= 0 or ((x - 1) & x) != 0 for x in sszes):
+        raise ValueError("sketchsize must be > 1 and a power of 2")
+    nt = args.nthreads
     assert os.path.isfile(fn), f"fnames argument ({fn}) does not exist"
     ntimes = args.nrepeat
-    print(f"##Results for {fn}")
-    print("#Method\tk\tSketchTime\tDistanceTime")
-    mdfile = "mashdest.msh"
-    mdstfile = "mashdist.out"
-    msout_fn, tsketch = repeat_x(bch_sketch_mash, args.nrepeat, fn, args.k, nt, mdfile, size=args.sketchsize)
-    msdistout_fn, tdist = repeat_x(bch_dist_mash, args.nrepeat, msout_fn, nt, mdstfile)
-    print(f"Mash\t{args.k}\t{tsketch}\t{tdist}")
+    import random
+    rstr = "".join(random.choice("abcdefgh") for i in range(12))
+    for nt in map(int, args.nthreads):
+        for k in map(int, args.k):
+            for ssz in sszes:
+                mdfile = f"MASHdest.k{k}.sz{ssz}.{rstr}msh"
+                mdstfile = f"MASHdist.k{k}.sz{ssz}.{rstr}out"
+                msout_fn, tsketch = repeat_x(bch_sketch_mash, args.nrepeat, fn, k, nt, mdfile, size=ssz)
+                msdistout_fn, tdist = repeat_x(bch_dist_mash, args.nrepeat, msout_fn, nt, mdstfile)
+                print(f"Mash\t{k}\t{ssz}\t8\t{nt}\t{tsketch}\t{tdist}")
+                for isbin in [1, 0]:
+                    d1out_fn, tsketch = repeat_x(bch_sketch_dashing, args.nrepeat, fn, k=k, threads=nt, size=ssz)
+                    d1distout_fn, tdist = repeat_x(bch_dist_dashing, args.nrepeat, fn, k=k, threads=nt, size=ssz, distdest=msdistout_fn.replace("mash", "dashing"), binary=isbin)
+                    print(f"Dashing1-{'bin' if isbin else 'txt'}\t{k}\t{ssz}\t1\t{nt}\t{tsketch}\t{tdist}")
+                for isbin in (True, False):
+                    for OP, D2S in zip([True, False], ("D2OP", "D2FSS")):
+                        for regsize in (8, 4, 2, 1, .5):
+                            OP2 = D2S + ("-bin" if isbin else "-txt")
+                            OP3 = OP2 + "-%g" % regsize
+                            distdest = f"d2dest.{OP3}.k{k}.{rstr}"
+                            d2out_fn, tsketch = repeat_x(bch_sketch_dashing2, args.nrepeat, fn, k=k, threads=nt, size=ssz, oneperm=OP)
+                            d2distout_fn, tdist = repeat_x(bch_dist_dashing2, args.nrepeat, fn, k=k, threads=nt, size=ssz, oneperm=OP, regsize=regsize, binary=isbin, distdest=distdest)
+                            print(f"{OP3}\t{k}\t{ssz}\t{regsize}\t{nt}\t{tsketch}\t{tdist}")
     return 0
 
 if __name__ == "__main__":
