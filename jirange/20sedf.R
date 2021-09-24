80572cf4df6a673a95b4f5a37e9e3d370e4346ce
Ben Langmead <ben.langmead@gmail.com>
2021-09-24 14:12:13 -0400

    add sum-of-absolute-error

--- jirange/20sedf.R
+++ jirange/20sedf.R
@@ -4,9 +4,7 @@ library(fst)
 library(tidyr)
 library(dplyr)
 
-se_df <- function(ani=F,
-                  bins=10,
-                  squared=T,
+se_df <- function(ani=F, bins=10,
                   exclude_types=c(),
                   include_types=c())
 {
@@ -16,7 +14,7 @@ se_df <- function(ani=F,
       mutate(bin=as.integer(bins * (mest*0.99999))/bins) %>%
       mutate(mdiff=(if(!!ani) { ani_diff } else { ji_diff })) %>%
       dplyr::group_by(k, totbits, bin, type) %>%
-      dplyr::summarise(sse=sum(mdiff**2), se=sum(mdiff), n=dplyr::n())
+      dplyr::summarise(sse=sum(mdiff**2), se=sum(mdiff), sae=sum(abs(mdiff)), n=dplyr::n())
     if(length(include_types) > 0) {
         dfsse <- dfsse %>% filter(type %in% include_types)
     } else if(length(exclude_types) > 0) {
@@ -37,42 +35,62 @@ incl_types_ani_lots <- c('Dash1', 'Mash', 'FSSN', 'FSS1', 'SS1',
                          'BMH8-50000000', 'BMH4-50000000', 'BMH2-50000000', 'BMH1-50000000', 'BMHN-50000000',
                          'BMH8-500000', 'BMH4-500000', 'BMH2-500000', 'BMH1-500000', 'BMHN-500000')
 
-tb <- se_df(ani=T, bins=1, include_types=incl_types_ani, squared=T) %>%
-  select(-se, -bin) %>%
+tb <- se_df(ani=T, bins=1, include_types=incl_types_ani) %>%
+  select(-se, -sae, -bin) %>%
   pivot_wider(names_from='type', values_from='sse')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ani_sse.tbl'))
 
-tb <- se_df(ani=T, bins=1, include_types=incl_types_ani, squared=F) %>%
-  select(-sse, -bin) %>%
+tb <- se_df(ani=T, bins=1, include_types=incl_types_ani) %>%
+  select(-sse, -sae, -bin) %>%
   pivot_wider(names_from='type', values_from='se')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ani_se.tbl'))
 
-tb <- se_df(ani=F, bins=1, include_types=incl_types_ji, squared=T) %>%
-  select(-se, -bin) %>%
+tb <- se_df(ani=T, bins=1, include_types=incl_types_ani) %>%
+  select(-se, -sse, -bin) %>%
+  pivot_wider(names_from='type', values_from='sae')
+capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ani_sae.tbl'))
+
+tb <- se_df(ani=F, bins=1, include_types=incl_types_ji) %>%
+  select(-se, -sae, -bin) %>%
   pivot_wider(names_from='type', values_from='sse')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ji_sse.tbl'))
 
-tb <- se_df(ani=F, bins=1, include_types=incl_types_ji, squared=F) %>%
-  select(-sse, -bin) %>%
+tb <- se_df(ani=F, bins=1, include_types=incl_types_ji) %>%
+  select(-sse, -sae, -bin) %>%
   pivot_wider(names_from='type', values_from='se')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ji_se.tbl'))
 
-tb <- se_df(ani=T, bins=20, include_types=incl_types_ani, squared=T) %>%
-  select(-se) %>%
+tb <- se_df(ani=F, bins=1, include_types=incl_types_ji) %>%
+  select(-se, -sse, -bin) %>%
+  pivot_wider(names_from='type', values_from='sae')
+capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ji_sae.tbl'))
+
+tb <- se_df(ani=T, bins=20, include_types=incl_types_ani) %>%
+  select(-se, -sae) %>%
   pivot_wider(names_from='type', values_from='sse')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ani_bins_sse.tbl'))
 
-tb <- se_df(ani=T, bins=20, include_types=incl_types_ani, squared=F) %>%
-  select(-sse) %>%
+tb <- se_df(ani=T, bins=20, include_types=incl_types_ani) %>%
+  select(-sse, -sae) %>%
   pivot_wider(names_from='type', values_from='se')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ani_bins_se.tbl'))
 
-tb <- se_df(ani=F, bins=20, include_types=incl_types_ji, squared=T) %>%
-  select(-se) %>%
+tb <- se_df(ani=T, bins=20, include_types=incl_types_ani) %>%
+  select(-se, -sse) %>%
+  pivot_wider(names_from='type', values_from='sae')
+capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ani_bins_sae.tbl'))
+
+tb <- se_df(ani=F, bins=20, include_types=incl_types_ji) %>%
+  select(-se, -sae) %>%
   pivot_wider(names_from='type', values_from='sse')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ji_bins_sse.tbl'))
 
-tb <- se_df(ani=F, bins=20, include_types=incl_types_ji, squared=F) %>%
-  select(-sse) %>%
+tb <- se_df(ani=F, bins=20, include_types=incl_types_ji) %>%
+  select(-sse, -sae) %>%
   pivot_wider(names_from='type', values_from='se')
 capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ji_bins_se.tbl'))
+
+tb <- se_df(ani=F, bins=20, include_types=incl_types_ji) %>%
+  select(-se, -sse) %>%
+  pivot_wider(names_from='type', values_from='sae')
+capture.output(print(tb, n=nrow(tb), width=200), file=paste0('20sedf_ji_bins_sae.tbl'))
