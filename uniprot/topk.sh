e522c4ce9fcfa4b2fa81eadd68a6310187a02ae3
Ben Langmead <ben.langmead@gmail.com>
2022-09-10 16:00:41 -0400

    more progress on section 3.5 experiments

--- uniprot/topk.sh
+++ uniprot/topk.sh
@@ -12,12 +12,29 @@ test -x "${DASH2}"
 
 # Ran on devlangmead1
 
+#
+# UNIPROT
+#
+
 # This is an attempt to recreate the experiment that generates the first KNN
 # graph mentioned in "All-pairs comparisons using LSH" section of Dashing 2
 # paper.
 
 /usr/bin/time -v \
   "${DASH2}" \
-  sketch --parse-by-seq --topk 256 -S256 -p80 --cmpout out.txt -k 5 --protein6 uniprot_sprot.fasta
+  sketch --parse-by-seq --topk 256 -S256 -p80 --cmpout uniprot_knn.tsv -k 5 --protein6 uniprot_sprot.fasta
 
 # Now need experiment that generates the graph using full all-pairs experiment.
+
+
+#
+# UNIREF 50
+#
+
+/usr/bin/time -v \
+  "${DASH2}" \
+  sketch --parse-by-seq --topk 256 -S256 -p80 --cmpout uniref50_knn.tsv -k 5 --protein6 uniref50.fasta
+
+# Now need experiment that generates the graph using full all-pairs experiment.
+# (This one has to get interrupted after a while)
+
