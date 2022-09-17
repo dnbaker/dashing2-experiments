404caa9cce048df27b95b7e6db8c732a7be7ba37
Ben Langmead <ben.langmead@gmail.com>
2022-09-17 17:38:31 -0400

    suggested changes to selbuckets

--- jirange/selbuckets.py
+++ jirange/selbuckets.py
@@ -1,16 +1,19 @@
 import numpy as np
 import numba
 import argparse as AP
-from sys import stderr, stdout
+from sys import stderr
+
+
 ap = AP.ArgumentParser()
 ap.add_argument("distmat", help="Path to a binary output of pairwise Jaccard similarities, in float32")
 ap.add_argument("--nbuckets", "-n", type=int, help="Number of buckets to divide the data into. Default: 100.", default=100)
-ap.add_argument("--fillnum", "-F", type=int, default=10)
+ap.add_argument("--fillnum", "-F", type=int, help="Default: 10.", default=10)
 ap = ap.parse_args()
 
 nb = ap.nbuckets
 mat = np.memmap(ap.distmat, np.float32)
 nelem = int(np.ceil(np.sqrt(np.prod(mat.shape) * 2)))
+
 
 @numba.cfunc("uint32(double)")
 def id2bkt(x):
@@ -29,10 +32,10 @@ indices = maketi(nelem)
 
 buckets = [[] for i in range(nb + 2)]
 
-cbkt = id2bkt(mat)
+mat_num_registers = len(mat)
 nbfull = 0
 fillnum = ap.fillnum
-for ci, cv in enumerate(cbkt):
+for ci, cv in enumerate(map(id2bkt, mat)):
     myb = buckets[cv]
     if len(myb) >= fillnum: continue
     myb.append(indices[ci])
@@ -40,13 +43,17 @@ for ci, cv in enumerate(cbkt):
         nbfull += 1
         print(f"{nbfull}/{len(buckets)}", file=stderr)
         if nbfull == len(buckets):
-            print(f"Finished all, breaking after {ci}/{len(cbkt)} {ci * 100./len(cbkt)}", file=stderr)
+            print(f"Finished all, breaking after {ci}/{mat_num_registers} {ci * 100./mat_num_registers}", file=stderr)
             break
 
 rstrs = ["0."]
 print("Bucket 0 [0.0]\t" + "\t".join(map(lambda x: "%d:%d" % (x[0], x[1]), buckets[0])))
+
+
 def idx2v(idx):
-    return (idx * (1. / nb), (idx + 1) * (1. / nb))
+    return idx * (1. / nb), (idx + 1) * (1. / nb)
+
+
 for idx, bk in enumerate(buckets[1:-1]):
     iv = idx + 1
     s, t = idx2v(idx)
