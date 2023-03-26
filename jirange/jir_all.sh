3c3d57144c1e16f2b8b249b11ce23f313ce71eba
Ben Langmead <ben.langmead@gmail.com>
2023-03-26 11:28:16 -0400

    fix missing space

--- jirange/jir_all.sh
+++ jirange/jir_all.sh
@@ -12,7 +12,7 @@ DATE="20230325"
 
 # 21-mers, 8 kbits
 name="${DATE}_k21_008kbit"
-python3 "${JIR_SCR}" bva_buckets.tsv bva_fastas.txt -k21 -s13 -S14 --name ${name} -o ${name}_out.txt--cpu 20
+python3 "${JIR_SCR}" bva_buckets.tsv bva_fastas.txt -k21 -s13 -S14 --name ${name} -o ${name}_out.txt --cpu 20
 # 21-mers, 32 kbits
 name="${DATE}_k21_032kbit"
 python3 "${JIR_SCR}" bva_buckets.tsv bva_fastas.txt -k21 -s15 -S16 --name ${name} -o ${name}_out.txt --cpu 20
