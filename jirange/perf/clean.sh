cd46bbd8817854b58058743fadd53faad09f9fb1
Ben Langmead <ben.langmead@gmail.com>
2023-06-30 11:59:04 -0400

    commit in perf

--- jirange/perf/clean.sh
+++ jirange/perf/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f *.sig *.opss *.ss *.pmh fastas.txt opss.txt ss.txt pmh.txt sm.np \
-  all.msh *.time *.labels.txt all.bindash all.bindash.*
+  all.msh *.time *.labels.txt all.bindash all.bindash.* *.fl32
