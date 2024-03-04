d22275fb16589dd497fcd3e6eb0badbdb714b9ef
Ben Langmead <ben.langmead@gmail.com>
2024-03-04 11:43:58 -0500

    complete set of clang11 files

---
+++ docker/clang11/build-aarch64.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build --platform linux/aarch64 -t dashing2-clang11-aarch64 .
---
+++ docker/clang11/run-amd64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+PLAT=amd64
+
+if [[ ! -d "dashing2-${PLAT}" ]] ; then
+  git clone --recursive https://github.com/dnbaker/dashing2 -- "dashing2-${PLAT}"
+fi
+nm=$(grep docker build-${PLAT}.sh  | awk '{print $6}')
+
+echo "When inside, run:"
+echo "cd /code && make 2>&1 | tee log-${PLAT}.txt"
+
+docker run -it -v`pwd`/dashing2-${PLAT}:/code --platform linux/${PLAT} ${nm}
